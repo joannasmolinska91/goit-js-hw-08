71528d9d0b47951a7a0085b596899ae6c5ced05e
joannasmolinska91 <122815691+joannasmolinska91@users.noreply.github.com>
2023-03-14 19:21:33 +0000

    Deploying to gh-pages from @ joannasmolinska91/goit-js-hw-08@54a4de88810bed904e4d71a002560a94919e5dc2 🚀

---
+++ 01-color-switcher.d3b7b610.js
@@ -0,0 +1,2 @@
+!function(){var t,e=document.querySelector("[data-start]"),n=document.querySelector("[data-stop]");function a(){document.body.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16))}e.addEventListener("click",(function(){e.disabled=!0,n.disabled=!1,t=setInterval(a,1e3)})),n.addEventListener("click",(function(){e.disabled=!1,n.disabled=!0,clearInterval(t)}))}();
+//# sourceMappingURL=01-color-switcher.d3b7b610.js.map
