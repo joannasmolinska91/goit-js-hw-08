54a4de88810bed904e4d71a002560a94919e5dc2
Joanna Smolińska-Iciek <joannasmolinska91@gmail.com>
2023-03-14 20:21:05 +0100

    colorSwitcher08

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,26 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 
+const startButton = document.querySelector('[data-start]');
+const stopButton = document.querySelector('[data-stop]');
+let intervalId;
+
+startButton.addEventListener('click', onStart);
+stopButton.addEventListener('click', onStop);
+
+function onStart() {
+  startButton.disabled = true;
+  stopButton.disabled = false;
+  intervalId = setInterval(changeColor, 1000);
+}
+
+function onStop() {
+  startButton.disabled = false;
+  stopButton.disabled = true;
+  clearInterval(intervalId);
+}
+
+function changeColor() {
+  document.body.style.backgroundColor = getRandomHexColor();
+}
